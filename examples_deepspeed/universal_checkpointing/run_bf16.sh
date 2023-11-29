39050681a7a4f46a38d8908f713064b08168a806
Sam Ade Jacobs <samjacobs@microsoft.com>
2023-11-29 10:52:35 -0800

    Extend universal checkpoint support for ds sequence parallelism (SP) and ZeRO stage 2

--- examples_deepspeed/universal_checkpointing/run_bf16.sh
+++ examples_deepspeed/universal_checkpointing/run_bf16.sh
@@ -3,8 +3,10 @@
 
 DIR=`pwd`
 DATETIME=`date +'date_%y-%m-%d_time_%H-%M-%S'`
-BASE_DATA_PATH=datasets
-DATASET=${BASE_DATA_PATH}/my-gpt2_text_document
+BASE_DATA_PATH=dataset/megatron_pile_dataset
+#data_path="BookCorpusDataset_text_document"
+#DATASET=${BASE_DATA_PATH}/gpt2_text_document
+DATASET=${BASE_DATA_PATH}/BookCorpusDataset_text_document
 VOCAB_PATH=${BASE_DATA_PATH}/gpt2-vocab.json
 MERGE_PATH=${BASE_DATA_PATH}/gpt2-merges.txt
 
@@ -13,7 +15,7 @@ script_path=$(realpath $0)
 script_dir=$(dirname $script_path)
 CONFIG_JSON="$script_dir/ds_config.json"
 
-ZERO_STAGE=1
+ZERO_STAGE=2
 DTYPE="bf16"
 
 # Debug
@@ -36,10 +38,11 @@ fi
 TP=2
 PP=2
 DP=2
-WORLD_SIZE=$((TP*PP*DP))
-GLOBAL_BATCH=16
+SP=1
+WORLD_SIZE=$((TP*PP*DP*SP))
+GLOBAL_BATCH=4
 MICRO_BATCH=$((GLOBAL_BATCH/WORLD_SIZE))
-TRAIN_ITERS=100000
+TRAIN_ITERS=1000
 LR=6.0e-3
 MIN_LR=6.0e-4
 
@@ -47,13 +50,14 @@ MIN_LR=6.0e-4
 LOAD_TP=$TP
 LOAD_PP=$PP
 LOAD_DP=$DP
+LOAD_SP=$SP
 RUN_TAG="save"
 # RUN_TAG="ref_load${LOAD_TP}_${LOAD_PP}_${LOAD_DP}"
 
-EXP_DIR="z${ZERO_STAGE}_uni_ckpt" 
-CHECKPOINT_PATH=${EXP_DIR}/checkpoints/gpt2/z${ZERO_STAGE}/$DTYPE/tp${TP}_pp${PP}_dp${DP}_${SIZE_TAG}
-LOAD_CHECKPOINT_PATH=${EXP_DIR}/checkpoints/gpt2/z${ZERO_STAGE}/$DTYPE/tp${LOAD_TP}_pp${LOAD_PP}_dp${LOAD_DP}_${SIZE_TAG}
-LOG_DIR="${EXP_DIR}/tensorboard/$DTYPE/tp${TP}_pp${PP}_dp${DP}_hd${HIDDEN}_nl${LAYERS}_gbsz${GLOBAL_BATCH}_mbsz${MICRO_BATCH}_z${ZERO_STAGE}_LR_${LR}_${MIN_LR}_${DTYPE}_${SIZE_TAG}_${RUN_TAG}"
+EXP_DIR="z${ZERO_STAGE}_uni_ckpt_v2" 
+CHECKPOINT_PATH=${EXP_DIR}/checkpoints/gpt2/z${ZERO_STAGE}/$DTYPE/tp${TP}_pp${PP}_dp${DP}_sp${SP}_${SIZE_TAG}
+LOAD_CHECKPOINT_PATH=${EXP_DIR}/checkpoints/gpt2/z${ZERO_STAGE}/$DTYPE/tp${LOAD_TP}_pp${LOAD_PP}_dp${LOAD_DP}_sp${LOAD_SP}_${SIZE_TAG}
+LOG_DIR="${EXP_DIR}/tensorboard/$DTYPE/tp${TP}_pp${PP}_dp${DP}_sp${SP}_hd${HIDDEN}_nl${LAYERS}_gbsz${GLOBAL_BATCH}_mbsz${MICRO_BATCH}_z${ZERO_STAGE}_LR_${LR}_${MIN_LR}_${DTYPE}_${SIZE_TAG}_${RUN_TAG}"
 mkdir -p $LOG_DIR
 
 while [[ $# -gt 0 ]]
@@ -77,6 +81,7 @@ done
 options=" \
 	--tensor-model-parallel-size $TP \
 	--pipeline-model-parallel-size $PP \
+	--ds-sequence-parallel-size $SP \
         --num-layers $LAYERS \
         --hidden-size $HIDDEN \
         --num-attention-heads 32 \
@@ -111,6 +116,8 @@ options=" \
 	--tensorboard-dir $LOG_DIR
         "
 
+#--no-pipeline-parallel \
+#--pipeline-model-parallel-size $PP \
 
 options="${options} \
         --deepspeed \
